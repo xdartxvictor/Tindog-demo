4838c5230833603c9b461d46fd8aab9674a64204
victor <xdartxvictor@gmail.com>
2018-01-23 11:19:52 -0500

    Initial Commit

---
+++ Tindog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tindog
+//
+//  Created by Victor Aliaga on 1/23/18.
+//  Copyright © 2018 Platzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
