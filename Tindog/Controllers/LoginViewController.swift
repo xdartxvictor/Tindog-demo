ba7598ba1124f23d890f2718f37f5feffc1ce68b
victor <xdartxvictor@gmail.com>
2018-02-03 10:51:53 -0500

    step-9 fin // en nuestro alert view

--- Tindog/Controllers/LoginViewController.swift
+++ Tindog/Controllers/LoginViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var loginCopyLbl: UILabel!
+    @IBOutlet weak var subLoginBtn: UIButton!
+    var registerMode = true
     
     @IBAction func closeBtn(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    func showAlert(title: String, message: String){
+        let alertView = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertView, animated: true, completion: nil)
+    }
+    
+    @IBAction func loginActionBtn(_ sender: Any) {
+        if self.emailTextField.text == "" || self.passwordTextField.text == ""{
+            self.showAlert(title: "Error", message: "Alguno de los campos esta Vacio")
+        }else{
+            if let email = self.emailTextField.text{
+                if let password = self.passwordTextField.text{
+                    if registerMode{
+                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil{
+                                self.showAlert(title: "Error", message: error!.localizedDescription)
+                            }else{
+                                print("cuenta creada")
+                            }
+                        })
+                    }else{
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func subLoginActionBtn(_ sender: Any) {
+        if self.registerMode{
+            self.loginBtn.setTitle("Login", for: .normal)
+            self.loginCopyLbl.text = "Eres Nuevo?"
+            self.subLoginBtn.setTitle("Registrate", for: .normal)
+            self.registerMode = false
+        }else{
+            self.loginBtn.setTitle("Crear Cuenta", for: .normal)
+            self.loginCopyLbl.text = "Ya tienes Cuenta?"
+            self.subLoginBtn.setTitle("Login", for: .normal)
+            self.registerMode = true
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
