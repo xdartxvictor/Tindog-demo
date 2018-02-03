5e2b69fdeb6469b9a62566ddf1423642d26eb8c5
victor <xdartxvictor@gmail.com>
2018-02-03 14:50:54 -0500

    [update] Profile logic

--- Tindog/Controllers/ProfileViewController.swift
+++ Tindog/Controllers/ProfileViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 import Firebase
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileDisplaynameLbl: UILabel!
+    @IBOutlet weak var profileEmailLbl: UILabel!
+    
     @IBAction func closeProfileBtn(_ sender: Any) {
         try! Auth.auth().signOut()
         self.dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.profileImage.layer.cornerRadius = self.profileImage.bounds.size.height / 2
+        self.profileImage.layer.borderColor = UIColor.white.cgColor
+        self.profileImage.layer.borderWidth = 1.0
+        self.profileImage.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
